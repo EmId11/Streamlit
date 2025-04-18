164280954b7073c403c9e11b9374a1bdc00ca1b1
EmId11 <idrisayman88@gmail.com>
2025-04-18 19:52:14 +1000

    Testing streamlit - first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hello World")
+st.subheader("Welcome to Streamlit")
+st.write("This is a simple Streamlit app.")
+st.text_input("Enter your name:", "")
+st.text_area("Enter your message:", "")
+st.button("Submit")
+
+
+
